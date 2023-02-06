7d48f126abe286ed26829f0ace61f5a2925ef677
felipebaguena <felipebaguena@gmail.com>
2023-02-06 18:49:01 +0100

    Teoría JS Semana 2

--- js/main.js
+++ js/main.js
@@ -161,3 +161,81 @@ como argumentos de una función cuando la ejecutamos.
 // for(let elemento in prendas){
 //     console.log(prendas[elemento]);
 // };
+
+
+////////////////////////////////////////////////////////////////
+
+// TUTORÍA 1 SEMANA 2
+
+////////////////////////////////////////////////////////////////
+
+//Declaración de una función
+
+/*
+Declaramos todos los pasos que la función va a hacer para realizarse.
+Esta declaración es previa a la ejecución, por lo tanto, declararla no implica
+que se tenga que ejecutar a no ser que se llame de forma voluntaria
+ */
+
+
+ //Ejemplo multilinea
+//  const Multiplicar = (parametro1, parametro2) => {
+
+//     return parametro1 * parametro2;
+
+//  };
+
+
+// //Ejemplo en una línea
+//  const Multiplicar = (parametro1, parametro2) => parametro1 * parametro2;
+
+
+
+
+//  //Ejecución de la función
+
+// let resultado = Multiplicar(3,5);
+
+// console.log(resultado);
+
+
+//FUNCIÓN QUE LLAMA A OTRA FUNCIÓN..........
+
+//Declaro la función saludar
+// const Saludar = (nombre) => console.log(`Hola ${nombre}`);
+
+// //Declaro la función para pedir el nombre
+// const PideNombre = () => {
+
+//     let pedimos = prompt("Dime un nombre");
+
+//     //Una vez tengo el nombre que me han dado por prompt, el siguiente paso es 
+//     //llamar a una función denominada Saludar, dándole el nombre que recogí antes como argumento
+//     Saludar(pedimos);
+// };
+
+// //Ejecuto la función que pide el nombre...ese es el primer paso
+// PideNombre();
+
+// let david = {
+
+//     edad: 27,
+//     ciudad: "Valencia",
+//     hobbie: "Programar",
+//     videoconsolas : ["Nintendo DS", "Nintendo 3DS", "Ps2", "Ps3", "Ps4"],
+//     mascotas : {
+//         mascota1 : "Vanitas",
+//         mascota2 : "Onix"
+//     }
+
+// }
+
+// console.log(david);
+
+// //Repaso de For in...
+
+// for(let elemento in david){
+//     console.log(david[elemento]);
+// }
+
+//For in es la única herramienta que puede recorrer un objeto de JS.
